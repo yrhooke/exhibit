9939ea273980c099dd651bc71c9d4fe1063d6d71
yrhooke <yotamvonreshef@gmail.com>
2019-09-24 21:17:11 +0300

    add form placeholders + make not required

--- exhibit/catalogue/forms.py
+++ exhibit/catalogue/forms.py
@@ -10,6 +10,15 @@ class WorkInExhibitionForm(forms.ModelForm):
 
 class ArtworkSearchForm(forms.ModelForm):
     """Search for Artworks"""
+
+    series = forms.ModelChoiceField(queryset=Series.objects.all(), required=False, empty_label="Series")
+    title = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': "Title"}))
+    location = forms.ModelChoiceField(queryset=Location.objects.all(), required=False, empty_label="Location")
+    status = forms.TypedChoiceField(choices=Artwork.OVERALL_STATUS_CHOICES, required=False, empty_value="Status")
+    size = forms.TypedChoiceField(choices=Artwork.OVERALL_STATUS_CHOICES, required=False, empty_value="Size Category")
+    year = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Year'}), required=False)
+    owner = forms.CharField(required=False)
+
     class Meta():
         model = Artwork
         fields = [
@@ -23,10 +32,16 @@ class ArtworkSearchForm(forms.ModelForm):
             'rolled',
             'owner',
         ]
-    
+
 
 class LocationSearchForm(forms.ModelForm):
     """Search for locations"""
+
+    name = forms.CharField(required=False)
+    address_1 = forms.CharField(required=False)
+    city = forms.CharField(required=False)
+    country = forms.CharField(required=False)
+
     class Meta():
         model = Location
         fields = [
@@ -43,6 +58,4 @@ class LocationSearchForm(forms.ModelForm):
 class ExhibitionSearchForm(forms.Form):
     """search for exhibitions"""
     exhibition = forms.ModelChoiceField(queryset=Exhibition.objects.all(), required=False,
-    label="Was in Exhibition")
-
-
+                                        label="Was in Exhibition", empty_label="")
