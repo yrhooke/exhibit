098f306fba71556a9ceea0ad6616b4d5c14d3a1a
yrhooke <yotamvonreshef@gmail.com>
2020-01-06 23:49:14 +0200

    non-functional login

--- exhibit/upload/scripts.py
+++ exhibit/upload/scripts.py
@@ -1,14 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-def log_in(domain):
-    login_url = f'{domain}/accounts/login'
-    client = requests.session()
-    # initiate session
-    c = client.get(login_url)
-    login_form_csrf_middleware_token = find_csrf_token_in_html(c.text, 'form.login')
-
-
 
 
 def find_csrf_token_in_html(html, form_selector):
@@ -26,11 +18,52 @@ def find_csrf_token_in_html(html, form_selector):
     csrf token or none
     """
     document = BeautifulSoup(html, 'html.parser')
-    soup = BeautifulSoup(html_string, 'html.parser') 
     # form = document.select('#object-details form')[0] 
-    form = document.select(form_selector)[0]
-    csrf_input = form.find(attrs={'name'='csrfmiddlewaretoken'})
+    try:
+        form = document.select(form_selector)[0]
+        csrf_input = form.find(attrs={'name':'csrfmiddlewaretoken'})
+    except Exception:
+        return document
     return csrf_input.attrs.get('value')
+
+def log_in(domain, username, password):
+    """Log in to website
+
+    Parameters
+    ----------
+    domain: string
+        domain of website
+    username: string
+        username for login
+    password: string
+        password for login
+    
+    Returns
+    -------
+    client : logged in requests.session
+    """
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:71.0) Gecko/20100101 Firefox/71.0'
+
+    login_url = f'{domain}/accounts/login'
+    client = requests.session()
+    # initiate session
+    c = client.get(login_url)
+    login_form_csrf_middleware_token = find_csrf_token_in_html(c.text, 'form.login')
+    login_form_data = {
+        'csrfmiddlewaretoken': login_form_csrf_middleware_token,
+        'login': username,
+        'password': password,
+        'next': '/',
+    }
+    r = client.post(login_url, data=login_form_data, headers=dict(Referer=login_url), cookies=client.cookies)
+    print(r.cookies)
+    # print(r.request.headers)
+    # print(r.request.cookies)
+    r.raise_for_status()
+    return client
+
+
+
 
 def extract_artwork_data_from_filename(filename):
     pass
@@ -49,4 +82,6 @@ def upload_artwork(domain_name, series, image_filename, sessionid):
     except Exception as e:
         print(f'error on {filename}')
         print(e)
+
+
 
